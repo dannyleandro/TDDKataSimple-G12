94edf612cbca1f08c797c44643b6242e4bfeea1b
ea.nunezt <ea.nunezt@uniandes.edu.co>
2018-09-30 18:12:01 -0500

    Ciclo TDD para que funcione con un string de N numeros (ROJO)

--- test_estadistica.py
+++ test_estadistica.py
@@ -9,9 +9,18 @@ class TestEstadistica(TestCase):
     def test_getArregloUnNumero(self):
             self.assertEqual(Estadistica().getArreglo("1"), [1], "Un numero")
 
-    #Ciclo TDD para que funcione con un string de 2 numeros
+    # Ciclo TDD para que funcione con un string de 2 numeros
 
     def test_getArregloDosNumeros(self):
         self.assertEqual(Estadistica().getArreglo("1,3"), [2], "Dos numeros")
+
+
+    # Ciclo TDD para que funcione con un string de N numeros
+
+    def test_getArregloNNumeros(self):
+        self.assertEqual(Estadistica().getArreglo("1,3,5"), [3], "Tres numeros")
+        self.assertEqual(Estadistica().getArreglo("1,3,5,45"), [4], "Cuatro numeros")
+        self.assertEqual(Estadistica().getArreglo("1,3,5,45,10"), [5], "Cinco numeros")
+
 
 
