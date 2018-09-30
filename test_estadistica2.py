7779622ad66d57a3c140f2c97baad23634b9a995
ea.nunezt <ea.nunezt@uniandes.edu.co>
2018-09-30 18:49:13 -0500

    Devolver un arreglo con el numero de elementos y el minimo (ROJO)

--- test_estadistica2.py
+++ test_estadistica2.py
@@ -15,3 +15,11 @@ class TestEstadistica(TestCase):
 
     def test_getArregloDosNumeros(self):
         self.assertEqual(Estadistica().getArreglo("1,3"), [2, 1], "Dos numeros, 1 y 1")
+
+
+    # Ciclo TDD para que funcione con un string de N numeros
+
+    def test_getArregloNNumeros(self):
+        self.assertEqual(Estadistica().getArreglo("6,7,3"), [3,3], "Tres numeros 3,1")
+        self.assertEqual(Estadistica().getArreglo("4,6,5,1"), [4,1], "Cuatro numeros 4,1")
+        self.assertEqual(Estadistica().getArreglo("9,8,2,45,10"), [5,2], "Cinco numeros 5,2")
