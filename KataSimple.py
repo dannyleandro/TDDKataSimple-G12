a74bb116a1a9ad8bb9c5992450af6de02f043d77
Danny Hurtado <dannylhurtado@gmail.com>
2018-09-30 19:47:51 -0500

    Devolver un arreglo con el número de elementos, el mínimo, el máximo y el promedio, String de N números(VERDE)

--- KataSimple.py
+++ KataSimple.py
@@ -4,20 +4,14 @@ class Estadistica:
         result = cadena.split(',')
         min = 9999
         max = 0
+        sum = 0
         if cadena == '':
             return [0, None, None, None]
-        elif len(result) == 1:
-            return [1, int(cadena), int(cadena), int(cadena)]
-        elif len(result) == 2:
-            if int(result[0]) > int(result[1]):
-                return [2, int(result[1]), int(result[0]), (int(result[0])+int(result[1]))/2]
-            else:
-                return [2, int(result[0]), int(result[1]), (int(result[0])+int(result[1]))/2]
         else:
             for x in result:
                 if int(x) < min:
                     min = int(x)
-            for x in result:
                 if int(x) > max:
                     max = int(x)
-            return [len(result), min, max]
+                sum = sum + int(x)
+            return [len(result), min, max, float(sum)/len(result)]
