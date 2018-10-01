74b971d4431f5f22f1a7a81dd213a8d9e0aab2b4
Danny Hurtado <dannylhurtado@gmail.com>
2018-09-30 19:17:32 -0500

    Devolver un arreglo con el número de elementos, el mínimo y el máximo, String de dos números(VERDE)

--- KataSimple.py
+++ KataSimple.py
@@ -7,9 +7,13 @@ class Estadistica:
             return [0, None, None]
         elif len(result) == 1:
             return [1, int(cadena), int(cadena)]
+        elif len(result) == 2:
+            if int(result[0]) > int(result[1]):
+                return [2, int(result[1]), int(result[0])]
+            else:
+                return [2, int(result[0]), int(result[1])]
         else:
             for x in result:
                 if int(x) < min:
                     min = int(x)
-
             return [len(result), min]
