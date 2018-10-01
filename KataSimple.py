c3b029e45d9a9b8b94a52d055bd3e3647445dbd8
Danny Hurtado <dannylhurtado@gmail.com>
2018-09-30 19:23:44 -0500

    Devolver un arreglo con el número de elementos, el mínimo y el máximo, String de N números(VERDE)

--- KataSimple.py
+++ KataSimple.py
@@ -3,17 +3,14 @@ class Estadistica:
     def getArreglo(self, cadena):
         result = cadena.split(',')
         min = 9999
+        max = 0
         if cadena == '':
             return [0, None, None]
-        elif len(result) == 1:
-            return [1, int(cadena), int(cadena)]
-        elif len(result) == 2:
-            if int(result[0]) > int(result[1]):
-                return [2, int(result[1]), int(result[0])]
-            else:
-                return [2, int(result[0]), int(result[1])]
         else:
             for x in result:
                 if int(x) < min:
                     min = int(x)
-            return [len(result), min]
+            for x in result:
+                if int(x) > max:
+                    max = int(x)
+            return [len(result), min, max]
