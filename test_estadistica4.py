3bc0c9198d8cd10460329d2579353ce3120e2404
ea.nunezt <ea.nunezt@uniandes.edu.co>
2018-09-30 19:30:50 -0500

    Devolver un arreglo con el numero de elementos, el minimo,  el maximo y el promedio. string de 1 numeros (ROJO)

--- test_estadistica4.py
+++ test_estadistica4.py
@@ -6,3 +6,6 @@ from KataSimple import Estadistica
 class TestEstadistica(TestCase):
     def test_getArreglo(self):
         self.assertEqual(Estadistica().getArreglo(""), [0, None, None, None], "cadena vacia")
+
+    def test_getArregloUnNumero(self):
+        self.assertEqual(Estadistica().getArreglo("1"), [1, 1, 1, 1], "Un numero Y UNO")
