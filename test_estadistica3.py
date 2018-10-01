d3aa8da84be17bd6a330964d748e6d33fd1b75b8
ea.nunezt <ea.nunezt@uniandes.edu.co>
2018-09-30 19:02:15 -0500

    Devolver un arreglo con el numero de elementos, el minimo y el maximo Ciclo TDD  para que funcione con el string vacio (ROJO)

--- test_estadistica3.py
+++ test_estadistica3.py
@@ -1,11 +1,7 @@
 from unittest import TestCase
 from KataSimple import Estadistica
 
-# Devolver un arreglo con el numero de elementos y el minimo
-
 # Devolver un arreglo con el numero de elementos, el minimo y el maximo
-
-
 class TestEstadistica(TestCase):
     def test_getArreglo(self):
         self.assertEqual(Estadistica().getArreglo(""), [0, None, None], "cadena vacia")
