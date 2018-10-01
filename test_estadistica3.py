f73ca734ba774731bdaf7a49dd70f624a0e7d86a
ea.nunezt <ea.nunezt@uniandes.edu.co>
2018-09-30 19:20:12 -0500

    Devolver un arreglo con el numero de elementos, el minimo y el maximo. string de n numeros (ROJO)

--- test_estadistica3.py
+++ test_estadistica3.py
@@ -14,3 +14,10 @@ class TestEstadistica(TestCase):
 
     def test_getArregloDosNumeros(self):
         self.assertEqual(Estadistica().getArreglo("1,3"), [2, 1, 3], "Dos numeros, 2 y 1,3")
+
+    # Ciclo TDD para que funcione con un string de N numeros
+
+    def test_getArregloNNumeros(self):
+        self.assertEqual(Estadistica().getArreglo("6,7,3"), [3, 3, 7], "Tres numeros ")
+        self.assertEqual(Estadistica().getArreglo("4,6,5,1"), [4, 1, 6], "Cuatro numeros ")
+        self.assertEqual(Estadistica().getArreglo("9,8,2,45,10"), [5, 2, 45], "Cinco numeros ")
